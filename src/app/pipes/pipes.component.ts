e89b6ea74ecfd74a071a127de695aba03988ea93
haresh.t <hareshthumar46@gmail.com>
2022-07-02 16:01:48 +0530

    pipe component add

--- src/app/pipes/pipes.component.ts
+++ src/app/pipes/pipes.component.ts
@@ -7,6 +7,24 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PipesComponent implements OnInit {
 
+  /*------ Example-1,2 -------*/
+  name = 'Angular Basic Pipe';
+
+  /*------ Example-3 -------*/
+  uname= 'title case pipe';
+
+  /*------ Example-3 -------*/
+  today = new Date();
+
+
+
+  user = {
+    name: 'haresh',
+    age: 25
+  }
+
+
+
   constructor() { }
 
   ngOnInit(): void {
