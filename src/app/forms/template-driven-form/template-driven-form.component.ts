a25975b70dc394ca0c71dd036f78481dffed52f4
Haresh Thumar <you@example.com>
2022-08-05 16:58:34 +0530

    template driven forms example

--- src/app/forms/template-driven-form/template-driven-form.component.ts
+++ src/app/forms/template-driven-form/template-driven-form.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, ViewChild, ElementRef, OnInit } from '@angular/core';
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-template-driven-form',
@@ -7,13 +8,46 @@ import { Component, OnInit } from '@angular/core';
 })
 export class TemplateDrivenFormComponent implements OnInit {
 
-  constructor() { }
+  // constructor() { }
 
   ngOnInit(): void {
   }
 
-  register(regForm: any) {
-    console.log(regForm);
+  // register(regForm: any) {
+  //   const fName = regForm.controls.fName.value;
+  //   const lName = regForm.controls.lName.value;
+  //   const email = regForm.controls.email.value;
+  //   console.log(regForm);
+  // }
+
+
+
+  /*=========================  =======================*/
+
+  countryList:country[] = [
+    new country("1", "India"),
+    new country('2', 'USA'),
+    new country('3', 'England')
+  ];
+
+  onSubmit(contactForm) {
+    console.log(contactForm.value);
   }
 
 }
+
+
+export class country {
+  id:string;
+  name:string;
+ 
+  constructor(id:string, name:string) {
+    this.id=id;
+    this.name=name;
+  }
+}
+
+
+/*-------------- mobile no. & email vallidation -----------------*/
+  // secondaryPhone: new FormControl('', [Validators.required, Validators.pattern("^[0-9]{10}$")]),
+  // primaryEmail: new FormControl('', [Validators.required, Validators.pattern('^[a-z0-9._%+-]+@[a-z0-9.-]+\\.[a-z]{2,4}$')])
